244481020a4902ebbf7e540c3cb10c0b721bc55f
Greem <orangenewyorkcreativeagency@gmail.com>
2020-04-27 19:11:24 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -9,7 +9,7 @@ end
 
 def add_element_to_end_of_array(array, element)
   array_ele = []
-  array_ele.push("element")
+  array_ele.push("element", "1", 2, 3)
 end
 
 def add_element_to_start_of_array(array, element)
